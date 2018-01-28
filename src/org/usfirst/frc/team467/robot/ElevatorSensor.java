e886f22e8c54f60c7becb1d4ee501193753b88a1
Jerry Du <jduomaha@gmail.com>
2018-01-27 19:57:16 -0500

    Updated the variables

--- src/org/usfirst/frc/team467/robot/ElevatorSensor.java
+++ src/org/usfirst/frc/team467/robot/ElevatorSensor.java
@@ -1,8 +1,9 @@
-package org.usfirst.frc.team467.robot;
+package org.eigusfirst.frc.team467.robot;
 
 import org.apache.log4j.Logger;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class ElevatorSensor {
 	private static ElevatorSensor instance = null;
@@ -10,18 +11,44 @@ public class ElevatorSensor {
 
 	private static final double TICKS_PER_TURN = 253.0;
 	
-	public static final double ELEVATOR_INITIAL_TICKS = 956.0;
+	private static final double GEAR_CIRCUMFERENCE_IN_INCHES = 10;
 			
-	public static final double ELEVATOR_MAX_HEIGHT_IN_INCHES = 120;
+	public static final double ELEVATOR_MAX_HEIGHT_IN_FEET = 10;
 	
-	public static final double ELEVATOR_MIN_HEIGHT_IN_INCHES = 0;
+	public static final double ELEVATOR_MIN_HEIGHT_IN_FEET = 0;
+
+	public static final double ELEVATOR_INITIAL_TICKS = 196;
 	
-	public static final double ELEVATOR_MAX_TURNS = 8.0;
+	public static final int ELEVATOR_HEIGHT_SENSOR_ID = 0;
+	
+	public static final int TALON_HEIGHT_CONTROLLER_ID = 1;
+	
+	
+	public enum Stops {
+		min(ELEVATOR_MIN_HEIGHT_IN_FEET),
+		fieldSwitch(2),
+		lowScale(6),
+		highScale(8);
+
+		public final double height;
+		
+		Stops(double height) {
+			this.height = height;
+		}
+	}
 	
 	private AnalogInput heightSensor;
+	
+	private WPI_TalonSRX heightController;
 
+	private double feetPerTick;
+	
+	private double previousNumber;
+	
 	private ElevatorSensor() {
-		heightSensor = new AnalogInput(0);
+		heightSensor = new AnalogInput(ELEVATOR_HEIGHT_SENSOR_ID);
+		heightController = new WPI_TalonSRX(TALON_HEIGHT_CONTROLLER_ID);
+		feetPerTick = GEAR_CIRCUMFERENCE_IN_INCHES / TICKS_PER_TURN / 12;
 	}
 	
 	/**
@@ -38,21 +65,43 @@ public class ElevatorSensor {
 		//1 rotation=253 ticks
 	}
 
+	/**
+	 * Moves based on the Xbox controller analog input
+	 * 
+	 * @param speed the speed and direction. Shall be a value between -1 and 1.
+	 */
+	public void manualMove(double speed) {
+		if (isOutOfRange()) {
+			speed = 0;
+			// TODO: Rumble here
+		}
+		double example = Stops.fieldSwitch.height;
+		for (Stops stop : Stops.values()) {
+			System.out.println(stop.height);
+		}
+		heightController.set(speed);
+	}
+
 	public boolean isOutOfRange() {
-		double height = heightSensor.getAverageValue();
+		double height = (heightSensor.getAverageValue() - ELEVATOR_INITIAL_TICKS) * feetPerTick;
 		
-		if (height/16.9 >= 176.3 || height/16.9 <= 56.6) {
+		if (getHeight()  > ELEVATOR_MAX_HEIGHT_IN_FEET || getHeight() < ELEVATOR_MIN_HEIGHT_IN_FEET) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	//Height/16.9 represents the height in inches. 
 	
-	public double getHeight() {
-		double height = heightSensor.getValue();
-		LOGGER.info("Height in inches: " + height/16.9);
-		return height;
+	public boolean garbageCan() {
+		if()		
 	}
 	
+	public double getHeight() {
+		double height = (heightSensor.getValue() - ELEVATOR_INITIAL_TICKS) * feetPerTick;
+		LOGGER.info("Height in feet: " + height);
+		return height;
+	}
 }
+	
+
+//TalonSRX ID is 1
