4bda56f891d573ba106167cbf5605942fbc5bb8b
Nathan Petrangelo <nathan@petrangelo.org>
2018-02-11 14:09:12 -0500

    Indentation

--- src/org/usfirst/frc/team467/robot/RobotMap.java
+++ src/org/usfirst/frc/team467/robot/RobotMap.java
@@ -12,12 +12,12 @@ public class RobotMap {
 	public enum RobotID {
 		PreseasonBot, Board, Competition_1, Competition_2
 	};
-	
+
 	public static final int FRONT_LEFT = 0;
 	public static final int FRONT_RIGHT = 1;
 	public static final int BACK_LEFT = 2;
 	public static final int BACK_RIGHT = 3;
-	
+
 	public static final int ELEVATOR_TICKS_PER_TURN = 253;
 	public static final double ELEVATOR_GEAR_CIRCUMFERENCE_IN_INCHES = 10;
 	public static final double ELEVATOR_MAX_HEIGHT_IN_FEET = 10;
@@ -26,11 +26,11 @@ public class RobotMap {
 	public static final int ELEVATOR_HEIGHT_SENSOR_ID = 0;
 	public static final int ELEVATOR_MOTOR_CHANNEL = 1;
 	public static final int MAX_ELEVATOR_RPM = 1000; //Not the real value, placeholder constant. 
-/*	* The lowest value is 196.0, the maximum value is 3741.0. The middle is 1968.5
+	/*	* The lowest value is 196.0, the maximum value is 3741.0. The middle is 1968.5
 	 * New max: 2980, new min:956.5
 	 * 16.9 ticks = 1 inch
 	 * 1 rotation=253 ticks */
-		
+
 	// Steering motor ids
 	// TODO: Enumerate steering motor IDS
 	public static boolean HAS_WHEELS;
@@ -75,22 +75,17 @@ public class RobotMap {
 			HAS_WHEELS = false;
 			WHEEL_ENCODER_CODES_PER_REVOLUTION = 256;
 
-			HAS_ELEVATOR = false;
-			HAS_RAMPS = false;
-
 			HAS_GRABBER = true;
 			GRABBER_L_CHANNEL = 0;
 			GRABBER_R_CHANNEL = 1;
 			OPTICAL_CHANNEL = 5;
 
-			// TODO Assign values to the game piece variables, and make more as appropriate
-			EVEVATOR_MOTOR_CHANNEL = 1;
-			RAMP_SOLENOID_CHANNEL = 0;
-			
 			HAS_ELEVATOR = true;
-			HAS_GRABBER = false;
-			HAS_RAMPS = false;
+			EVEVATOR_MOTOR_CHANNEL = 1;
 			
+			HAS_RAMPS = false;
+			RAMP_SOLENOID_CHANNEL = 0;
+
 			isDriveMotorInverted = new boolean[] { false, true, false, true };
 			break;
 		case Competition_1:
