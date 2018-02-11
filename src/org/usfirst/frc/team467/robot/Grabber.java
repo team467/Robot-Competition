9f87c839f2c708fadc6b9a3f63145e1ee9d956e2
Nathan Petrangelo <nathan@petrangelo.org>
2018-02-10 21:30:43 -0500

    Indent

--- src/org/usfirst/frc/team467/robot/Grabber.java
+++ src/org/usfirst/frc/team467/robot/Grabber.java
@@ -1,4 +1,5 @@
 package org.usfirst.frc.team467.robot;
+
 import org.apache.log4j.Logger;
 
 import edu.wpi.first.wpilibj.Spark;
@@ -7,11 +8,11 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class Grabber {
 	private static final Logger LOGGER = Logger.getLogger(Grabber.class);
 
-    private static Grabber instance;
-    private SpeedController left;
+	private static Grabber instance;
+	private SpeedController left;
 	private SpeedController right;
 	OpticalSensor os;
-	
+
 	private Grabber() {
 		if (RobotMap.HAS_GRABBER) {
 			left = new Spark(RobotMap.GRABBER_L_CHANNEL);
@@ -23,20 +24,20 @@ public class Grabber {
 			os = OpticalSensor.getInstance();
 		}
 	}
-	
+
 	public static Grabber getInstance() {
-		if(instance == null) {
-			instance = new Grabber(); 
+		if (instance == null) {
+			instance = new Grabber();
 		}
-		
+
 		return instance;
 	}
-	
+
 	public void grab(double throttle) {
 		if (!RobotMap.HAS_GRABBER) {
 			return;
 		}
-		
+
 		if (Math.abs(throttle) < RobotMap.MIN_GRAB_SPEED) {
 			throttle = 0.0;
 		}
@@ -44,7 +45,7 @@ public class Grabber {
 		left.set(throttle * RobotMap.MAX_GRAB_SPEED);
 		right.set(-throttle * RobotMap.MAX_GRAB_SPEED);
 	}
-	
+
 	public boolean hasCube() {
 		return RobotMap.HAS_GRABBER && os.detectedTarget();
 	}
