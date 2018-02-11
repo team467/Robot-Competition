065150f73d18b8b13a3087ede9fef94f94732210
Nathan Petrangelo <nathan@petrangelo.org>
2018-02-10 21:31:54 -0500

    Optical Sensor Indents

--- src/org/usfirst/frc/team467/robot/OpticalSensor.java
+++ src/org/usfirst/frc/team467/robot/OpticalSensor.java
@@ -4,21 +4,21 @@ import edu.wpi.first.wpilibj.DigitalInput;
 public class OpticalSensor {
 	private static OpticalSensor instance;
 	private DigitalInput di;
-	
+
 	private OpticalSensor() {
 		di = new DigitalInput(RobotMap.OPTICAL_CHANNEL);
 	}
-	
+
 	public static OpticalSensor getInstance() {
 		if (instance == null) {
 			instance = new OpticalSensor();
 		}
-		
+
 		return instance;
 	}
-	
+
 	public boolean detectedTarget() {
 		return di.get();
 	}
-	
+
 }
