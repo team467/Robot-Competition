37bf37f01530f44319b31ab40af178e2f5016236
mcen21 <en.matthew.c@gmail.com>
2018-02-12 19:37:15 -0500

    Fixed comments, variable values to be decided later in build season. Class description also added.

--- src/org/usfirst/frc/team467/robot/TiltMonitor.java
+++ src/org/usfirst/frc/team467/robot/TiltMonitor.java
@@ -1,15 +1,13 @@
 package org.usfirst.frc.team467.robot;
 
 import org.apache.log4j.Logger;
-/*Class description: Tilt monitor monitors the tilting of the robot using the 
- * gyrometer and will retract the elevator at a certain point
+/**
+ * TiltMonitor monitors the tilting of the robot using the 
+ * gyrometer and will retract the elevator if the robot tips too much.
 */
 public class TiltMonitor {
 	private static TiltMonitor instance;
 	private static final Logger LOGGER = Logger.getLogger(TiltMonitor.class);
-	
-	public static final int FORWARD_PANIC_ANGLE = 45;
-	public static final int BACKWARD_PANIC_ANGLE = -45;
 	
 	public static TiltMonitor getInstance() {
 		if (instance == null) {
@@ -18,8 +16,8 @@ public class TiltMonitor {
 		return instance;
 	}
 	public void periodic() {
-		if (Gyrometer.getInstance().getPitchDegrees() > FORWARD_PANIC_ANGLE || 
-			Gyrometer.getInstance().getPitchDegrees() < BACKWARD_PANIC_ANGLE) {
+		if (Gyrometer.getInstance().getPitchDegrees() > RobotMap.FORWARD_PANIC_ANGLE || 
+			Gyrometer.getInstance().getPitchDegrees() < RobotMap.BACKWARD_PANIC_ANGLE) {
 			LOGGER.debug("Pitch=" + Gyrometer.getInstance().getPitchDegrees());
 			Elevator.getInstance().moveToHeight(Elevator.Stops.floor);
 		}
