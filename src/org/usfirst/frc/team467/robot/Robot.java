3769479d528a83aac1a5e20acdb3640cf8a4ab9a
Nathan Petrangelo <nathan@petrangelo.org>
2018-02-19 21:47:24 -0500

    No telemetry in disabled

--- src/org/usfirst/frc/team467/robot/Robot.java
+++ src/org/usfirst/frc/team467/robot/Robot.java
@@ -95,8 +95,6 @@ public class Robot extends TimedRobot {
 		LOGGER.trace("Disabled Periodic");
 		driverstation.readInputs();
 		driverstation.logJoystickIDs();
-		elevator.telemetry();
-		//LOGGER.debug("Right: "	+drive.getRightDistance() + " Left: " + drive.getLeftDistance());
 	}
 	//TODO: Figure out the NetworkTables later.
 	//	String[] autoList = {"none", "go"};
