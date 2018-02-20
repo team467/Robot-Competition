0d89594b4ce40309a2b1fcaa680f74890b7d9bbf
Bryan Duerk <bryanduerk@users.noreply.github.com>
2018-02-19 19:08:25 -0500

    Changed back to arcade drive

--- src/org/usfirst/frc/team467/robot/DriverStation.java
+++ src/org/usfirst/frc/team467/robot/DriverStation.java
@@ -96,7 +96,8 @@ public class DriverStation {
 	 */
 	public DriveMode getDriveMode() {
 		// TODO: Set the drive mode based on the buttons pushed
-		return DriveMode.CurvatureDrive; // Update with the correct drive mode
+//		return DriveMode.CurvatureDrive; // Update with the correct drive mode
+		return DriveMode.ArcadeDrive; // Update with the correct drive mode
 	}
 
 	public boolean getTerminateAuto() {
