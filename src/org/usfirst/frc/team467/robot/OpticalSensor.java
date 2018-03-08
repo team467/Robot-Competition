82bb7bd2b3601ff694a83467d22e232378a02001
Bryan Duerk <bryanduerk@users.noreply.github.com>
2018-03-08 15:49:35 -0500

    Checked for similator usage to remove null pointer exceptions.

--- src/org/usfirst/frc/team467/robot/OpticalSensor.java
+++ src/org/usfirst/frc/team467/robot/OpticalSensor.java
@@ -8,19 +8,25 @@ public class OpticalSensor {
 
 	private OpticalSensor() {
 		System.out.println(RobotMap.OPTICAL_CHANNEL);
-		di = new DigitalInput(RobotMap.OPTICAL_CHANNEL);
+		if (!RobotMap.useSimulator) {
+			di = new DigitalInput(RobotMap.OPTICAL_CHANNEL);
+		}
 	}
 
 	public static OpticalSensor getInstance() {
-		if (instance == null && !RobotMap.useSimulator) {
+		if (instance == null) {
 			instance = new OpticalSensor();
-		}
+		} 
 
 		return instance;
 	}
 
 	public boolean detectedTarget() {
-		return !di.get();
+		if (!RobotMap.useSimulator) {
+			return !di.get();
+		} else {
+			return false;
+		}
 	}
 
 }
