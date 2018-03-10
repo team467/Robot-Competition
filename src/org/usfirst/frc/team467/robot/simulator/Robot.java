36bc95b837b0f721e47d613b1e87b05894c97e54
Bryan Duerk <bryanduerk@users.noreply.github.com>
2018-03-10 15:50:18 -0500

    Added 1/2 foot offset to starting position, lines up with objects better in simulator

--- src/org/usfirst/frc/team467/robot/simulator/Robot.java
+++ src/org/usfirst/frc/team467/robot/simulator/Robot.java
@@ -62,12 +62,14 @@ public class Robot {
 		drive.zero();
 		matchConfig.load();
 		
+		double offset = 0.492;
+		
 		if (SimulatedData.autoMode.startsWith("Left")) {
-			data.startingLocation(2.5, 0);
+			data.startingLocation(2.5+offset, 0);
 		} else if (SimulatedData.autoMode.startsWith("Center")) {
-			data.startingLocation(12.5, 0);
+			data.startingLocation(12.5+offset, 0);
 		} else { // Right
-			data.startingLocation(21.58, 0);
+			data.startingLocation(21.58+offset, 0);
 		}		
 		data.send();
 
