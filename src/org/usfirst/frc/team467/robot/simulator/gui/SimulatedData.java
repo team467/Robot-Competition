cae1dcbdd573e9fe1e4cedee401ccfb037daf85e
Bryan Duerk <bryanduerk@users.noreply.github.com>
2018-03-09 20:25:33 -0500

    Add struct type class for holding simulated data

---
+++ src/org/usfirst/frc/team467/robot/simulator/gui/SimulatedData.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team467.robot.simulator.gui;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+/**
+ *
+ */
+public class SimulatedData {
+	
+	public static String gameSpecificMessage = "LLL";
+
+	public static Alliance teamColor = Alliance.Red;
+
+	public static String autoMode = "Left";
+}
