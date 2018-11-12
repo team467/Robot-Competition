28a7d7c8025f5184104e5fdefe0d6bbbeb06d96b
Bryan Duerk <bryanduerk@users.noreply.github.com>
2018-11-12 16:55:33 -0500

    Moved the get position to the static init.

--- src/test/java/frc/robot/drive/motorcontrol/pathtracking/FieldPositionTest.java
+++ src/test/java/frc/robot/drive/motorcontrol/pathtracking/FieldPositionTest.java
@@ -2,27 +2,35 @@ package frc.robot.drive.motorcontrol.pathtracking;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Test ability to derive field position from sensor data ")
 public class FieldPositionTest {
 
-    FieldPosition fieldPosition;
+    private static FieldPosition fieldPosition;
+
+    @BeforeAll
+    @DisplayName("Initialize field position for test")
+    public static void initAll() {
+        fieldPosition = FieldPosition.getInstance();
+        fieldPosition.init(0, 0, 90.0, 0.0, 0, 0);
+    }
 
     @Test
     @DisplayName("Test simple forward movement")
     public void positionAfter3FootForwardMovementShouldIncrement3FootOnYAxisTest() {
-        FieldPosition position = FieldPosition.getInstance();
-        position.init(0, 0, 90.0, 0.0, 0, 0);
-
         double fakeSensorReadingLeft =  3;
         double fakeSensorReadingRight =  3;
-        position.update(fakeSensorReadingLeft, fakeSensorReadingRight);
+        fieldPosition.update(fakeSensorReadingLeft, fakeSensorReadingRight);
 
         double tolerance = 0.01;
-        assertEquals(0.0, position.x(), tolerance);
-        assertEquals(3.0, position.y(), tolerance);
+        assertEquals(0.0, fieldPosition.x(), tolerance);
+        assertEquals(3.0, fieldPosition.y(), tolerance);
     }
+
+    
 
 }
