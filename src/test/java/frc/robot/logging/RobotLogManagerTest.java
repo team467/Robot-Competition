e78fc6d0a675524518c2658d33e640ac37c7e58f
Andrew L <>
2019-01-25 21:18:44 -0500

    Create RobotLogManagerTest.java
    matt and hasan changes

---
+++ src/test/java/frc/robot/logging/RobotLogManagerTest.java
@@ -0,0 +1,38 @@
+package frc.robot.logging;
+
+import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.fail;
+
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class RobotLogManagerTest {
+    private String testDirectoryFile = "C:\\Users\\Team467\\Documents\\GitHub\\Robot2019-Competition\\src\\main\\deploy\\log4j2.yaml";
+    private static Logger LOGGER = RobotLogManager.getTestLogger(RobotLogManagerTest.class.getName());
+    
+    @BeforeClass
+    public static void initAll() {
+    }
+
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void directoryTest() {
+        System.out.println(RobotLogManager.getDirectory(testDirectoryFile));
+        LOGGER.trace("Completed test");
+        assertTrue(true);
+    }
+
+    // Commented out so that test passes
+    // @Test
+    // public void failingTest() {
+    //   fail("a failing test");
+    // }
+
+}
+
