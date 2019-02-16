15d62929dafa0166ec8809f81f2010a68317aa6f
Bryan Duerk <bryanduerk@users.noreply.github.com>
2019-02-16 12:10:27 -0500

    Moved vision controller to vision package

---
+++ src/main/java/frc/robot/vision/VisionController.java
@@ -0,0 +1,75 @@
+package frc.robot.vision;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import frc.robot.RobotMap;
+import frc.robot.logging.RobotLogManager;
+import frc.robot.usercontrol.DriverStation467;
+
+import org.apache.logging.log4j.Logger;
+
+public class VisionController {
+
+  private static VisionController instance = null;
+
+  private static final Logger LOGGER 
+      = RobotLogManager.getMainLogger(VisionController.class.getName());
+
+  private NetworkTable vision;
+  private DriverStation467 driverStation;
+
+  /**
+   * Returns a singleton instance of the telemery builder.
+   * 
+   * @return TelemetryBuilder the telemetry builder instance
+   */
+  public static VisionController getInstance() {
+    if (instance == null) {
+      instance = new VisionController();
+    }
+    return instance;
+  }
+
+  private VisionController() {
+    vision = NetworkTableInstance.getDefault().getTable("vision");  
+    driverStation = DriverStation467.getInstance();
+  }
+
+  public double angle() {
+    double angle = vision.getEntry("angle").getDouble(-1000);
+    LOGGER.debug("Angle from network table is {}", angle);
+    return angle;
+  }
+
+  /**
+   * Sets the feedback for the navigator, including LEDs and rumble.
+   */
+  public void navigatorFeedback() {
+    double angle = angle();
+    boolean haveAngle = vision.getEntry("haveAngle").getBoolean(false);
+    if (haveAngle) {
+      if (Math.abs(angle) <= RobotMap.ON_TARGET) {
+        // TODO: Debug Log
+        // TODO: LED FUNCTION
+        // TODO: RUMBLE
+      } else if (Math.abs(angle) <= RobotMap.ANGLE_OFFSET_LEVEL_ONE) {
+        // TODO: Debug Log
+        // TODO: LED FUNCTION
+        // TODO: RUMBLE
+      } else if (Math.abs(angle) <= RobotMap.ANGLE_OFFSET_LEVEL_TWO) {
+        // TODO: Debug Log
+        // TODO: LED FUNCTION
+      } else if (Math.abs(angle) <= RobotMap.ANGLE_OFFSET_LEVEL_THREE) {
+        // TODO: Debug Log
+        // TODO: LED FUNCTION
+      } else if (Math.abs(angle) <= RobotMap.ANGLE_OFFSET_LEVEL_FOUR) {
+        // TODO: Debug Log
+        // TODO: LED FUNCTION
+      } else {
+        // TODO: Debug Log
+      }
+    }
+  }
+
+}
