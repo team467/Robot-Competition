7ebb9034922280364f5c7231f4ee31d955c87929
Andrew Friedman <arfrie22@students.shrewsbury.k12.ma.us>
2019-02-15 21:27:15 -0500

    Changed talon mode to velocity

--- src/main/java/frc/robot/drive/Drive.java
+++ src/main/java/frc/robot/drive/Drive.java
@@ -243,9 +243,9 @@ public class Drive extends DifferentialDrive implements AutoDrive {
     LOGGER.debug("Target: L: {} R: {} Current L: {} R: {}", 
         df.format(leftDistance), df.format(rightDistance), 
         df.format(getLeftDistance()), df.format(getRightDistance()));
-    left.set(ControlMode.Position, feetToTicks(leftDistance));
+    left.set(ControlMode.Velocity, feetToTicks(leftDistance));
     // The right motor is reversed
-    right.set(ControlMode.Position, feetToTicks(rightDistance));
+    right.set(ControlMode.Velocity, feetToTicks(rightDistance));
     data.updateDrivePosition(getLeftDistance(), getRightDistance());
     fieldState.update(getLeftDistance(), getRightDistance());
   }
@@ -343,9 +343,9 @@ public class Drive extends DifferentialDrive implements AutoDrive {
     double leftDistTicks = feetToTicks(moveLeftDistance);
     double rightDistTicks = feetToTicks(moveRightDistance);
 
-    left.set(ControlMode.Position, leftDistTicks);
+    left.set(ControlMode.Velocity, leftDistTicks);
     // The right motor is reversed
-    right.set(ControlMode.Position, rightDistTicks);
+    right.set(ControlMode.Velocity, rightDistTicks);
 
     data.updateDrivePosition(getLeftDistance(), getRightDistance());
     fieldState.update(getLeftDistance(), getRightDistance());
