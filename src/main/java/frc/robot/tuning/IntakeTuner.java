106fb6736fb677b56809a31c0aad081a772e44ad
Bryan Duerk <bryanduerk@users.noreply.github.com>
2019-02-28 23:46:12 -0500

    Added an intake tuner to test arm up and down in test periodic mode.

---
+++ src/main/java/frc/robot/tuning/IntakeTuner.java
@@ -0,0 +1,39 @@
+package frc.robot.tuning;
+
+import frc.robot.gamepieces.CargoIntake;
+import frc.robot.gamepieces.CargoIntake.CargoIntakeArm;
+import frc.robot.logging.RobotLogManager;
+
+import org.apache.logging.log4j.Logger;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class IntakeTuner implements Tuner {
+
+  private static final Logger LOGGER
+      = RobotLogManager.getMainLogger(IntakeTuner.class.getName());
+
+  CargoIntake intake;
+
+  IntakeTuner() {
+    intake = CargoIntake.getInstance();
+  }
+
+  public void init() {
+    LOGGER.info("Init Intake Tuning");
+  }
+
+  public void periodic() {
+    String tuningValue = SmartDashboard.getString("DB/String 5", "OFF");
+    LOGGER.info("Tuning Value: {}", tuningValue);
+    try {
+      CargoIntakeArm command = CargoIntakeArm.valueOf(tuningValue.toUpperCase());
+      intake.arm(command);
+      LOGGER.debug("Intake state: {}", intake.arm().toString());
+    } catch (IllegalArgumentException e) {
+      LOGGER.error("Need to enter one of UP, DOWN or OFF int DB/String 5.");
+      LOGGER.error(e);
+    }
+  }
+
+}
