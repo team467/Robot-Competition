b27786e046e1f2cd2c409dd532cc4872569ddfa9
Junyang-L <58312669+Junyang-L@users.noreply.github.com>
2020-02-01 20:36:41 -0500

    added the ability to set climber

--- src/main/java/frc/robot/gamepieces/Climber.java
+++ src/main/java/frc/robot/gamepieces/Climber.java
@@ -19,30 +19,34 @@ import org.apache.logging.log4j.Logger;
 import com.revrobotics.*;
 
 public class Climber extends GamePieceBase implements GamePiece {
-
+    //logger
     private static final Logger LOGGER = RobotLogManager.getMainLogger(Climber.class.getName());
-
+    //inst reresenting climber
     private static Climber instance = null;
-
+    //motors
     private static CANSparkMax climbLeader;
     private static CANSparkMax climbFollower;
     private static SparkMaxSpeedControllerGroup climbGroup;
-
+    //status of robot
+    private ClimberStatus status;
+//constructor
     private Climber(SparkMaxSpeedControllerGroup climber) { // constructor
         super("Telemetry", "Climber");
     }
-
+    
+    //gets the instance
     public static Climber getInstance() {
-
+        //creates new instance if none exists
         if (instance == null) {
             if (RobotMap.HAS_CLIMBER) {
+                //instantiates clomber motors
                 climbLeader = new CANSparkMax(RobotMap.CLIMB_MOTER_LEADER, MotorType.kBrushless);
                 climbFollower = null;
 
                 if (RobotMap.HAS_CLIMBFOLLOWER) {
                     climbFollower = new CANSparkMax(RobotMap.CLIMB_MOTER_FOLLOWER, MotorType.kBrushless);
                 }
-
+                //creates control group
                 climbGroup = new SparkMaxSpeedControllerGroup("Climber", ControlType.kVelocity, RobotMap.CLIMBER_SENSOR,
                         RobotMap.motorIsInverted, climbLeader, climbFollower);
 
@@ -50,7 +54,7 @@ public class Climber extends GamePieceBase implements GamePiece {
                 climbGroup = new SparkMaxSpeedControllerGroup();
             }
             instance = new Climber(climbGroup);
-
+            
             instance.stop();
 
         }
@@ -58,16 +62,45 @@ public class Climber extends GamePieceBase implements GamePiece {
     }
 
     public void stop() {
-        //to be filled in with anything if nessecary
+        // stops the climber so that the robot doesnt move
         climbGroup.set(0.0);
     }
 
+    //method to make the climber move up or down
+    public void set(ClimberStatus status) {
+        this.status = status;
+    }
+
+    public enum ClimberStatus {
+        UP, DOWN, STOP;
+
+        public void actuate() {
+            if (RobotMap.HAS_CLIMBER) {
+                switch (this) {
+                case STOP:
+                default:
+                    climbGroup.set(0.0);
+                    LOGGER.debug("Climber Stopped");
+                    break;
+                case UP:
+                    climbGroup.set(1.0);
+                    LOGGER.debug("Climber Going Up");
+                    break;
+                case DOWN:
+                    climbGroup.set(-1.0);
+                    LOGGER.debug("Climber Going Down");
+                    break;
+                }
+            }
+        }
+    }
+
     public void periodic() {
-        //figure out inputs TODO
-        //switch block deciding how to power motors TODO
+        // figure out inputs TODO
+        // switch block deciding how to power motors TODO
         if (RobotMap.HAS_CLIMBER) {
             if (enabled) {
-                
+                status.actuate();
             } else {
                 stop();
             }
