cb491142a40ca9795a22d0d3f132a511a4e06a0a
Mcen21 <en.matthew.c@gmail.com>
2020-02-08 15:03:46 -0500

    Comment on SmartDashboard for  Indexer

--- src/main/java/frc/robot/sensors/IndexerSensors.java
+++ src/main/java/frc/robot/sensors/IndexerSensors.java
@@ -1,33 +1,33 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// /*----------------------------------------------------------------------------*/
+// /* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+// /* Open Source Software - may be modified and shared by FRC teams. The code   */
+// /* must be accompanied by the FIRST BSD license file in the root directory of */
+// /* the project.                                                               */
+// /*----------------------------------------------------------------------------*/
 
-package frc.robot.sensors;
+// package frc.robot.sensors;
 
-import frc.robot.RobotMap;
-import frc.robot.logging.RobotLogManager;
-import org.apache.logging.log4j.Logger;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import frc.robot.RobotMap;
+// import frc.robot.logging.RobotLogManager;
+// import org.apache.logging.log4j.Logger;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class IndexerSensors {
+// public class IndexerSensors {
     
-    private static final Logger LOGGER= RobotLogManager.getMainLogger(IndexerSensors.class.getName());
+//     private static final Logger LOGGER= RobotLogManager.getMainLogger(IndexerSensors.class.getName());
 
-    indexerSensors() {
+//     indexerSensors() {
 
-    }
+//     }
 
-        public void init() {
-            SmartDashboard.putBoolean("Object detect", false);
-            SmartDashboard .putNumber(key, value);
-        }
+//         public void init() {
+//             SmartDashboard.putBoolean("Object detect", false);
+//             SmartDashboard .putNumber(key, value);
+//         }
 
-        public void periodic() {
-            double tof = SmartDashboard.getBoolean(key, defaultValue)
+//         public void periodic() {
+//             double tof = SmartDashboard.getBoolean(key, defaultValue)
 
 
-        }
-}
+//         }
+// }
