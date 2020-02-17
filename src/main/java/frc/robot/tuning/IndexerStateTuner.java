58d6cf6f5290c3be8e78af1d8f845e3a7b68007e
Mcen21 <en.matthew.c@gmail.com>
2020-02-17 18:29:39 -0500

    Working IndexerSM

--- src/main/java/frc/robot/tuning/IndexerStateTuner.java
+++ src/main/java/frc/robot/tuning/IndexerStateTuner.java
@@ -14,6 +14,7 @@ import frc.robot.gamepieces.GamePieceController;
 import frc.robot.gamepieces.AbstractLayers.IndexerAL;
 import frc.robot.logging.RobotLogManager;
 import frc.robot.gamepieces.AbstractLayers.IndexerAL.SensorTestMode;
+import frc.robot.gamepieces.GamePieceController.DriverInput;
 
 
 
@@ -35,6 +36,9 @@ public class IndexerStateTuner implements Tuner {
         SmartDashboard.putNumber("Chamber Distance", 0);
         SmartDashboard.putBoolean("mouthSensor", false);
         SmartDashboard.putBoolean("chamberSensor", false);
+        SmartDashboard.putBoolean("forwardInput", false);
+        SmartDashboard.putBoolean("reverseInput", false);
+        SmartDashboard.putBoolean("setAuto", false);
 
         gamePieceController = GamePieceController.getInstance();
 
@@ -47,9 +51,20 @@ public class IndexerStateTuner implements Tuner {
         boolean mouthSensor = SmartDashboard.getBoolean("mouthSensor", false);
         boolean chamberSensor = SmartDashboard.getBoolean("chamberSensor", false);
 
-        gamePieceController.IndexAuto = true;
+        boolean driverForwardInput = SmartDashboard.getBoolean("forwardInput", false);
+        boolean driverReverseInput = SmartDashboard.getBoolean("reverseInput", false);
+
+        boolean indexToAuto = SmartDashboard.getBoolean("setAuto", false);
 
         gamePieceController.periodic();
+
+        if (indexToAuto == true) {
+            gamePieceController.autoInput(DriverInput.FORCE_AUTO_TRUE);
+            gamePieceController.IndexAuto = true;
+        }  else {
+            gamePieceController.autoInput(DriverInput.FORCE_AUTO_FALSE);
+            gamePieceController.IndexAuto = false;
+        }
 
         if (mouthSensor == true) {
             LOGGER.debug("mouthSensor is true");
@@ -63,12 +78,22 @@ public class IndexerStateTuner implements Tuner {
             indexer.setForceBallInChamber(SensorTestMode.FORCE_TRUE);
         } else {
             indexer.setForceBallInChamber(SensorTestMode.FORCE_FALSE);
+        }
+    
+        if (driverForwardInput == true) {
+            LOGGER.debug("mouthSensor is true");
+            gamePieceController.cellsForward(DriverInput.FORCE_TRUE);
+        } else {
+            gamePieceController.cellsForward(DriverInput.FORCE_FALSE);
+        }
+
+        if (driverReverseInput == true) {
+            LOGGER.debug("ChamberSensor is true");
+            gamePieceController.cellsReverse(DriverInput.FORCE_TRUE);
+        } else {
+            gamePieceController.cellsReverse(DriverInput.FORCE_FALSE);
         
         }
-        // if(shoot){
-
-        // }
-
         SmartDashboard.putNumber("Mouth Distance", indexer.getMouthDistance());
         SmartDashboard.putNumber("Chamber Distance", indexer.getChamberDistance());
     }
